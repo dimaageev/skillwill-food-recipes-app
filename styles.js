2234ddaaf362d8911a43468798728eb828aff696
Beka Batmanashvili <Bekabatmanashvili1@gmail.com>
2023-01-13 21:35:39 +0400

    feat: project starter constants

---
+++ styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  shadow: {
+    shadowColor: "black",
+    shadowOpacity: 0.37,
+    shadowOffset: { width: 0, height: 6 },
+    shadowRadius: 10,
+    shadowRadius: 7.49,
+  },
+});
