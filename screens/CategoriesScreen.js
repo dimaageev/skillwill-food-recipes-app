03071d6d8461c6518d67c8c608c6296bc9ece398
Beka Batmanashvili <Bekabatmanashvili1@gmail.com>
2023-01-17 12:13:10 +0400

    feat: categories screen

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {FlatList, StyleSheet, Text} from 'react-native'
+import CategoryGridItem from '../components/CategoryGridItem'
+import {CATEGORIES} from '../data/dummy-data'
+
+const CategoriesScreen = (props) => {
+   const renderGridItem = (itemData) => {
+      return (
+         <CategoryGridItem
+            title={itemData.item.title}
+            color={itemData.item.color}
+            onSelect={() => {
+               props.navigation.navigate('CategoryMeals', {
+                  categoryId: itemData.item.id,
+                  title: itemData.item.title
+               })
+            }}
+         />
+      )
+   }
+
+   return (
+      <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
+   )
+}
+
+const styles = StyleSheet.create({
+   gridItem: {
+      flex: 1,
+      margin: 15,
+      height: 150
+   }
+})
+
+export default CategoriesScreen
