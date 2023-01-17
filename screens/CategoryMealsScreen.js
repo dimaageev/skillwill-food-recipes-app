86350525b76af5d51d9c63bcc5c9bbc88dd2158c
Beka Batmanashvili <Bekabatmanashvili1@gmail.com>
2023-01-17 12:15:31 +0400

    fix: category meals screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
-import {useSelector} from 'react-redux'
 import MealList from '../components/MealList'
 import {CATEGORIES} from '../data/dummy-data'
 
 const CategoryMealsScreen = (props) => {
    const {categoryId} = props.route.params
-
-   const availableMeals = useSelector((state) => state.meals.filteredMeals)
-
-   const displayedMeals = availableMeals.filter(
-      (meal) => meal.categoryIds.indexOf(categoryId) >= 0
-   )
-
-   const selectedCategory = CATEGORIES.find((cat) => cat.id === categoryId)
 
    //    React.useLayoutEffect(() => {
    //       props.navigation.setOptions({
@@ -21,15 +12,7 @@ const CategoryMealsScreen = (props) => {
    //       })
    //    }, [props.navigation])
 
-   if (displayedMeals.length === 0) {
-      return (
-         <View style={styles.content}>
-            <Text>No meals found, maybe check your filters ❓</Text>
-         </View>
-      )
-   }
-
-   return <MealList listData={displayedMeals} navigation={props.navigation} />
+   return <MealList listData={[]} navigation={props.navigation} />
 }
 
 const styles = StyleSheet.create({
