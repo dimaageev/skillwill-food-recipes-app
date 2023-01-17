270ef9a21663c4e645dbf805bd6262ff39dcb74a
Beka Batmanashvili <Bekabatmanashvili1@gmail.com>
2023-01-17 12:06:29 +0400

    feat: components

---
+++ components/CategoryGridItem.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import {
+   View,
+   Text,
+   StyleSheet,
+   TouchableOpacity,
+   TouchableNativeFeedback,
+   Platform
+} from 'react-native'
+
+const CategoryGridItem = (props) => {
+   let TouchableComponent = TouchableOpacity
+
+   if (Platform.OS === 'android' && Platform.Version >= 21) {
+      TouchableComponent = TouchableNativeFeedback
+   }
+
+   return (
+      <View style={styles.touchable}>
+         <TouchableOpacity style={styles.gridItem} onPress={props.onSelect}>
+            <View
+               style={{...styles.container, ...{backgroundColor: props.color}}}>
+               <Text style={styles.title} numberOfLines={2}>
+                  {props.title}
+               </Text>
+            </View>
+         </TouchableOpacity>
+      </View>
+   )
+}
+
+const styles = StyleSheet.create({
+   touchable: {
+      flex: 1
+   },
+   gridItem: {
+      flex: 1,
+      margin: 15,
+      height: 150,
+      borderRadius: 10,
+      overflow: 'hidden',
+      elevation: 15
+   },
+   container: {
+      flex: 1,
+      borderRadius: 10,
+      shadowColor: 'black',
+      shadowOpacity: 0.37,
+      shadowOffset: {width: 0, height: 6},
+      shadowRadius: 10,
+      shadowRadius: 7.49,
+      padding: 15,
+      justifyContent: 'flex-end',
+      alignItems: 'flex-end'
+   },
+   title: {
+      fontWeight: 'bold',
+      fontSize: 22
+   }
+})
+
+export default CategoryGridItem
